7205e38082e0b6791cc809d3fef8afb7b5748f37
pedro87654321 <picanceladaf@udistrital.edu.co>
2024-03-08 07:48:47 -0500

    tvpedro2

--- src/televisor/aplicaciontv.java
+++ src/televisor/aplicaciontv.java
@@ -1,10 +1,30 @@
 package televisor;
-
+import java.util.Scanner;
 public class aplicaciontv {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner ler = new Scanner(System.in);
+		clasetv tv1 = new clasetv ();
+		int encendido;
+		System.out.println("ingrese la marca del televisor ");
+		tv1.setMarca(ler.nextLine());
+		System.out.println("ingrese tamaño del televisosr");
+		tv1.setTamaño(ler.nextInt());
+		System.out.println("(1)encendido, (2)apagado");
+		encendido=ler.nextInt();
+		if(encendido==1)
+		{
+		tv1.setEncendido(true);
+		}
+		else if(encendido==2)
+		{
+		tv1.setEncendido(false);
+		}
+        System.out.println("eliga entradas"+"\n (HDMI)"+"\n (AV)"+"\n (TV)");
+        tv1.setEntradas(ler.nextLine());
+        
+		
 	}
 
 }
