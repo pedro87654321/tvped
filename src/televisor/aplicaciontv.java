ac4acb9cf5251d8e908504d8a17bccc0c19962e6
pedro87654321 <picanceladaf@udistrital.edu.co>
2024-03-08 07:36:48 -0500

    tvpedro

---
+++ src/televisor/aplicaciontv.java
@@ -0,0 +1,10 @@
+package televisor;
+
+public class aplicaciontv {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/televisor/clasetv.java
@@ -0,0 +1,5 @@
+package televisor;
+
+public class clasetv {
+
+}
