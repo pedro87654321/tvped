3b7ab7adf70f58fdb982ec3671abb7a357738bc0
pedro87654321 <picanceladaf@udistrital.edu.co>
2024-03-08 07:52:27 -0500

    tvpedro3

--- src/televisor/clasetv.java
+++ src/televisor/clasetv.java
@@ -1,5 +1,49 @@
 package televisor;
 
 public class clasetv {
+	
+	String marca;
+	int tamaño;
+	boolean encendido;
+	String entradas;
+	int volumen;
+    int canal;
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public int getTamaño() {
+		return tamaño;
+	}
+	public void setTamaño(int tamaño) {
+		this.tamaño = tamaño;
+	}
+	public boolean isEncendido() {
+		return encendido;
+	}
+	public void setEncendido(boolean encendido) {
+		this.encendido = encendido;
+	}
+	public String getEntradas() {
+		return entradas;
+	}
+	public void setEntradas(String entradas) {
+		this.entradas = entradas;
+	}
+	public int getVolumen() {
+		return volumen;
+	}
+	public void setVolumen(int volumen) {
+		this.volumen = volumen;
+	}
+	public int getCanal() {
+		return canal;
+	}
+	public void setCanal(int canal) {
+		this.canal = canal;
+	}
+   
 
 }
